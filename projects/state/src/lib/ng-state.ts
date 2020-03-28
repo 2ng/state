5252aa34c1b19aa206e0a4692b2a60d6f2af31b6
Andrey Podkovyrov <a.podkovyrov@yandex.ru>
2020-03-28 22:20:05 +0300

    refactor: mv getChanges to func, fix init method

--- projects/state/src/lib/ng-state.ts
+++ projects/state/src/lib/ng-state.ts
@@ -2,28 +2,16 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { distinctUntilChanged, map, pluck } from 'rxjs/operators';
 import isEqual from '@2utils/is-equal';
 
-export interface State<T extends { [key: string]: any } = any> {
-  get: () => T;
-  changes: <K>(keyOrFn?: keyof T | ((state: T) => K)) => Observable<K>;
-}
-
 export class NgState<T extends { [key: string]: any } = any> {
   static isEqualFn: (a: any, b: any) => boolean = isEqual;
 
   private state$ = new BehaviorSubject<T>({} as T);
 
-  state: State<T> = {
+  state = {
     get: () => this.state$.value,
-    changes: keyOrFn => this.changes(keyOrFn)
+    changes: <K>(keyOrFn?: keyof T | ((state: T) => K)): Observable<K> =>
+      getChanges(this.state$, keyOrFn as string)
   };
-
-  constructor(initialState: T) {
-    this.init(initialState);
-  }
-
-  init(initialState: T) {
-    this.setState(initialState);
-  }
 
   protected setState = (partialState: Partial<T>): void => {
     this.state$.next({
@@ -32,17 +20,28 @@ export class NgState<T extends { [key: string]: any } = any> {
     });
   }
 
-  private changes(keyOrFn?: keyof T | ((state: T) => any)): Observable<any> {
-    let changes: Observable<any> = this.state$;
-
-    if (typeof keyOrFn === 'string') {
-      changes = changes.pipe(pluck(keyOrFn));
-    }
-
-    if (typeof keyOrFn === 'function') {
-      changes = changes.pipe(map(state => keyOrFn(state)));
-    }
-
-    return changes.pipe(distinctUntilChanged(NgState.isEqualFn));
+  constructor(private initialState: T) {
+    this.init();
   }
+
+  init() {
+    this.setState(this.initialState);
+  }
+}
+
+function getChanges<K>(
+  observable: Observable<any>,
+  keyOrFn?: string | ((state: K) => K)
+): Observable<K> {
+  let changes = observable;
+
+  if (typeof keyOrFn === 'string') {
+    changes = changes.pipe(pluck(keyOrFn));
+  }
+
+  if (typeof keyOrFn === 'function') {
+    changes = changes.pipe(map(state => keyOrFn(state)));
+  }
+
+  return changes.pipe(distinctUntilChanged(NgState.isEqualFn));
 }
