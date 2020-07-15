57e1883b935d81c03a2e445247cf2767de934892
Andrey Podkovyrov <a.podkovyrov@yandex.ru>
2020-07-16 00:54:34 +0300

    feat(user): add behavior subject example service

---
+++ projects/example/src/app/components/user/service/behavior-subject.service.ts
@@ -0,0 +1,50 @@
+import isEqual from '@2utils/is-equal';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { UserService } from '~components/user/service/user.service';
+import { UserFacade } from '~components/user/user.facade';
+import { Loading } from '~shared/services/http/class/loading/loading';
+import { LoadingStatus } from '~shared/services/http/class/loading/loading.interface';
+
+@Injectable()
+export class UserBehaviorSubjectService implements UserFacade {
+  private _name = new BehaviorSubject('andrey');
+  name$ = this._name.asObservable().pipe(distinctUntilChanged());
+
+  private _photo = new BehaviorSubject<string>(null);
+  photo$ = this._photo.asObservable().pipe(distinctUntilChanged());
+
+  private _loading = new BehaviorSubject<LoadingStatus>(Loading.idle().status);
+  loading$ = this._loading.asObservable().pipe(distinctUntilChanged(isEqual));
+
+  get name() {
+    return this._name.value;
+  }
+
+  private loadPhotoEffect = new Subject();
+
+  constructor(private userService: UserService) {
+    this.loadPhotoEffect
+      .pipe(switchMap(() => this.userService.loadUser$()))
+      .subscribe(({ status, response }) => {
+        if (response) {
+          this._photo.next(response.photo);
+        }
+
+        this._loading.next(status);
+      });
+  }
+
+  uppercase() {
+    this._name.next(this._name.value.toUpperCase());
+  }
+
+  lowercase() {
+    this._name.next(this._name.value.toLowerCase());
+  }
+
+  loadPhoto() {
+    this.loadPhotoEffect.next();
+  }
+}
--- projects/example/src/app/store/store.ts
+++ projects/example/src/app/store/store.ts
@@ -4,9 +4,11 @@ import { UserFacade } from '~components/user/user.facade';
 import { CounterStore } from './counter.store';
 import { ThemeStore } from './theme.store';
 import { UserStore } from './user.store';
+import { UserBehaviorSubjectService } from '~components/user/service/behavior-subject.service';
 
 export const STORE = [
   { provide: UserFacade, useClass: UserStore },
+  // { provide: UserFacade, useClass: UserBehaviorSubjectService },
   { provide: CounterFacade, useClass: CounterStore },
   { provide: ThemeFacade, useClass: ThemeStore },
 ];
