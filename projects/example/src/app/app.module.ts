874b32feee70c6f64136d63c6056f6e8070a9740
Andrey Podkovyrov <a.podkovyrov@yandex.ru>
2020-07-19 13:05:04 +0300

    chore: add example component with store

--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CounterComponent } from '~components/counter/counter.component';
 import { ThemeTogglerComponent } from '~components/theme-toggler/theme-toggler.component';
 import { UserUIComponent } from '~components/user-ui/user.component';
-import { UserComponent } from '~components/user/user.component';
+import { UserWFacadeComponent } from '~components/user/user.component';
 import { STORE } from '~store/store';
 import { AppComponent } from './app.component';
 
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent,
     CounterComponent,
-    UserComponent,
+    UserWFacadeComponent,
     UserUIComponent,
     ThemeTogglerComponent,
   ],
--- projects/example/src/app/components/user/user.component.ts
+++ projects/example/src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { UserStore } from './store/user.store';
 import { UserFacade } from './user.facade';
 
 @Component({
@@ -7,7 +8,7 @@ import { UserFacade } from './user.facade';
   styleUrls: ['./user.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserComponent {
+export class UserWFacadeComponent {
   name$ = this._facade.name$;
   photo$ = this._facade.photo$;
   loading$ = this._facade.loading$;
@@ -24,5 +25,28 @@ export class UserComponent {
 
   loadPhoto() {
     this._facade.loadPhoto();
+  }
+}
+// Для примера
+export class UserWStoreComponent {
+  name$ = this._store.name$;
+  photo$ = this._store.photo$;
+  loading$ = this._store.loading$;
+
+  constructor(private _store: UserStore) {}
+
+  uppercase() {
+    const { UPPERCASE } = this._store;
+    this._store.dispatch(UPPERCASE);
+  }
+
+  lowercase() {
+    const { LOWERCASE } = this._store;
+    this._store.dispatch(LOWERCASE);
+  }
+
+  loadPhoto() {
+    const { LOAD_PHOTO } = this._store;
+    this._store.dispatch(LOAD_PHOTO);
   }
 }
