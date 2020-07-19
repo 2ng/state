a5c53892e1cc8b92e4a6509bfa0a5f68355aad6a
Andrey Podkovyrov <a.podkovyrov@yandex.ru>
2020-07-19 12:52:23 +0300

    Revert "chore: del unused"
    This reverts commit 8d55f41dba8bf3091b44f285a0cfc3814e11487d.

---
+++ projects/state/src/lib/ng-state.ts
@@ -0,0 +1,62 @@
+import isEqual from '@2utils/is-equal';
+import { BehaviorSubject, Observable, of, Subject, Subscription } from 'rxjs';
+import { distinctUntilChanged, exhaustMap, map, tap } from 'rxjs/operators';
+
+export type Action = (actions: Observable<void>) => Observable<any>;
+
+interface Dispatch<T = any> {
+  action: Action;
+  payload?: T;
+}
+
+export class NgState<T extends { [key: string]: any } = any> {
+  private _state = new BehaviorSubject<T>({} as T);
+  private _actions = new Subject<Dispatch>();
+
+  private _actions$$: Subscription = this._actions
+    .pipe(
+      exhaustMap(({ action, payload }) => action(of(payload))),
+      tap(partialState => this.setState(partialState))
+    )
+    .subscribe();
+
+  /** @action Устанавливает начальное состояние */
+  INIT = this.pipe(map((state: T) => state));
+
+  constructor(initialState: T) {
+    this.dispatch(this.INIT, initialState);
+  }
+
+  get state(): T {
+    return this._state.value;
+  }
+
+  protected setState(partialState: Partial<T>): void {
+    this._state.next({ ...this.state, ...partialState });
+  }
+
+  protected select<K>(mappedFn?: (state: T) => K): Observable<K> {
+    return mappedFn
+      ? this._state.pipe(
+          map(state => mappedFn(state)),
+          distinctUntilChanged(isEqual)
+        )
+      : this._state.asObservable().pipe(distinctUntilChanged(isEqual));
+  }
+
+  dispatch(action: Action, payload?: any) {
+    this._actions.next({ action, payload });
+  }
+
+  protected pipe(...operations): Action {
+    return (actions: Observable<void>) =>
+      // @ts-ignore
+      actions.pipe(...operations);
+  }
+
+  destroy(): void {
+    this._actions$$.unsubscribe();
+    this._actions.complete();
+    this._state.complete();
+  }
+}
---
+++ projects/state/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ng-store
+ */
+
+export * from './lib/ng-action';
+export * from './lib/ng-state';
